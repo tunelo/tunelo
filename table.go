2af5032509dc5336fa91521eddb21fad5d4d23ba
Emiliano A. Billi <emiliano.billi@gmail.com>
2024-10-23 20:44:17 -0300

    v0.1

--- table.go
+++ table.go
@@ -1,14 +1,14 @@
 /*
- * This file is part of YAVA (YAVA, Another VPN Application).
+ * This file is part of Tunelo (Tunelo, Another VPN Application).
  *
  *
- * YAVA is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+ * Tunelo is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
- * YAVA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * Tunelo is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with YAVA. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with Tunelo. If not, see <http://www.gnu.org/licenses/>.
  *
  * Author: Emiliano A. Billi emiliano.billi@gmail.com
  * Date: 2024
