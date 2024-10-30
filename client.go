cf33bf3ed7d221a6a986d79169dece9081b4bfcb
Emiliano A. Billi <emiliano.billi@gmail.com>
2024-10-30 09:10:06 -0300

    Display

--- client.go
+++ client.go
@@ -19,6 +19,8 @@ package tunelo
 import (
 	"fmt"
 	"net"
+	"strings"
+	"time"
 
 	"github.com/tunelo/sudp"
 	"github.com/tunelo/utun"
@@ -49,6 +51,28 @@ func NewVnetClient(cird string, peer string, laddr *sudp.LocalAddr, raddr *sudp.
 	return &vnetc, nil
 }
 
+func display(anim bool, peer string, tun string, tunip string, mtu int) {
+
+	if anim {
+		go func() {
+			const col = 20
+			position := 0
+			direction := 1
+			for {
+				bar := strings.Repeat(" ", position) + "0" + strings.Repeat(" ", col-position-1)
+				fmt.Printf("\r[%s] Connected (%s), tun: %s, inet %s, mtu: %d  Ctr-C to exit ", bar, peer, tunip, mtu)
+				time.Sleep(100 * time.Millisecond)
+				position += direction
+				if position == col-1 || position == 0 {
+					direction *= -1
+				}
+			}
+		}()
+	} else {
+		fmt.Printf("[%s] Connected (%s), tun: %s, inet %s, mtu: %d  Ctr-C to exit ", peer, tunip, mtu)
+	}
+}
+
 func (c *VnetClient) Run() error {
 	var (
 		n int
@@ -56,6 +80,8 @@ func (c *VnetClient) Run() error {
 	)
 	err := make(chan error)
 	buf := make([]byte, c.Tun.MTU)
+
+	display(true, c.sock.RemoteAddr(), c.Tun.IP, c.Tun.MTU)
 
 	go func() {
 		for {
--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/tunelo/tunelo
 go 1.22.4
 
 require (
-	github.com/tunelo/sudp v0.0.0-20241029193940-dded525f045c
-	github.com/tunelo/utun v0.0.0-20241023164223-14eec6175686
+	github.com/tunelo/sudp v0.0.0-20241030120527-17aba6b2230d
+	github.com/tunelo/utun v0.0.0-20241030120506-2baad668cb81
 	golang.org/x/net v0.30.0
 )
 
