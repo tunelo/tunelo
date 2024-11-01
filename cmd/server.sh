a0bfb1e736b105c99a3a74cefd495c18e69b5fef
Emiliano A. Billi <emiliano.billi@gmail.com>
2024-11-01 15:08:42 -0300

    Update server.sh

--- cmd/server.sh
+++ cmd/server.sh
@@ -9,12 +9,12 @@ echo "-----------------------------"
 echo " Manual Configuration Steps  "
 echo "-----------------------------"
 echo "1. Enable MASQUERADE (eth0 is and example, use your physical interface)"
-echo "  $ iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE"
+echo "  $ sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE"
 echo "2. Enable IP Forward"
-echo "  $ echo 1 > /proc/sys/net/ipv4/ip_forward"
+echo "  $ sudo echo 1 > /proc/sys/net/ipv4/ip_forward"
 
 
-echo \
+./tunelo \
     -sudp_endpoint $SUDP_ENDPOINT \
     -sudp_pri $SUDP_PRI \
     -sudp_pub $SUDP_PUB \
